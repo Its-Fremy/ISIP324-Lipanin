700a7d04da8ed3b8e50e4e71a6dce9b052160728
Its-Fremy <dimalipa6@gmail.com>
2026-09-11 13:17:38 +0300

    Ввод кол-ва операций

--- ISIP324-Lipanin/Program.cs
+++ ISIP324-Lipanin/Program.cs
@@ -10,6 +10,22 @@ namespace ISIP324_Lipanin
     {
         static void Main(string[] args)
         {
+            int kolvo;
+            while (true)
+            {
+                Console.Write("Введите кол-во операций (от 2 до 40): ");
+                kolvo = int.Parse(Console.ReadLine());
+                if (kolvo >= 2 && kolvo <= 40)
+                {
+                    break;
+                }
+                Console.WriteLine("Введено неверное кол-во операций \nВведите от 2 до 40");
+            }
+
+        }
+        static void function()
+        {
+
         }
     }
 }
